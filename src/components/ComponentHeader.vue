49d9ee0b4c0e68a5935a688f08c50ba7ef4f76df
Irene289 <esmuybonita18@gmail.com>
2022-06-16 12:05:59 +0800

    feat: components setup

---
+++ src/components/ComponentHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header> 
+    Header clicked: {{ headerCount }} 
+    <button> Count up </button>
+    <ComponentNav />
+  </header>
+</template>
+
+<script>
+import ComponentNav from './ComponentNav.vue'
+
+export default {
+  name: "ComponentHeader",
+  components: {
+    ComponentNav
+  },
+  data() {
+    return {
+      headerCount: 0
+    }
+  }
+};
+</script>
---
+++ src/components/ComponentNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    Nav clicked: {{ navCount }}
+    <button>Count up</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "ComponentNav",
+  data() {
+    return {
+      navCount: 0
+    };
+  },
+};
+</script>
